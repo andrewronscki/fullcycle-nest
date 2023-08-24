bb03a104197fedbdefbe43da285f9ac805a5707e
André Wronscki Ricardo <andrewronsckiricardo@Andres-MacBook-Pro.local>
2023-08-24 18:00:07 -0300

    feat: adicao de websocket

---
+++ src/routes/routes/routes.gateway.ts
@@ -0,0 +1,23 @@
+import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { RoutesDriverService } from '../routes-driver/routes-driver.service';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class RoutesGateway {
+  constructor(private routesDriverService: RoutesDriverService) {}
+
+  @SubscribeMessage('new-points')
+  async handleMessage(
+    client: Socket,
+    payload: { route_id: string; lat: number; lng: number },
+  ): Promise<void> {
+    await this.routesDriverService.createOrUpdate(payload);
+
+    client.broadcast.emit('admin-new-points', payload);
+    client.broadcast.emit(`new-points/${payload.route_id}`, payload);
+  }
+}
